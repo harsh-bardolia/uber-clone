166395da69cab490fdc01b0eee748c7dc0bfbb6b
abhishekbardolia <asutra2019@gmail.com>
2022-10-22 14:33:21 +0530

    Login Unrequired

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
         });
       } else {
         setUser(null);
-        router.push("/login");
+        // router.push("/login");
       }
     });
   }, []);
